2ac14eae5c523a243ff040c5eb8979b044174dbe
tomasz awramski <tomasz.awramski@fujitsu.com>
2023-03-09 08:47:11 +0100

    feat: persistence fabric init commit
    Signed-off-by: tomasz awramski <tomasz.awramski@fujitsu.com>

---
+++ packages/cactus-plugin-persistence-fabric-block/README.md
@@ -0,0 +1,24 @@
+# `@hyperledger/cactus-plugin-fabric-block-persistance`
+
+## Prometheus Exporter
+This class creates a cactus-plugin-fabric-block-persistance.
+
+### Usage 
+
+### Prometheus Integration
+To use with this exporter make sure to install 
+
+```(yaml)
+
+```
+
+
+### Helper code
+
+###### response.type.ts
+
+
+###### data-fetcher.ts
+
+###### metrics.ts
+
---
+++ packages/cactus-plugin-persistence-fabric-block/src/main/typescript/public-api.ts
@@ -0,0 +1,17 @@
+import * as OpenApiJson from "../json/openapi.json";
+export { OpenApiJson };
+export { PluginPersistenceFabric } from "./plugin-factory-fabric-persistence-block";
+export { PluginPersistenceFabricBlock } from "./plugin-fabric-persistence-block";
+/*
+export {
+  GetConsortiumEndpointV1,
+  IGetConsortiumJwsEndpointOptions,
+} from "./";
+
+export {
+  GetNodeJwsEndpoint,
+  IGetNodeJwsEndpointOptions,
+} from "./";
+
+import { IPluginFactoryOptions } from "@hyperledger/cactus-core-api";
+*/
