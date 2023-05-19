0b328df72f7f2fbefb2af1867b65245f3a7387fb
tomasz awramski <tomasz.awramski@fujitsu.com>
2023-05-19 19:25:22 +0200

    fix: fix
    Signed-off-by: tomasz awramski <tomasz.awramski@fujitsu.com>

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
-# country=$(sort-package-json "package.json" --check )
-sort-package-json "**/cactus-*/package.json" --check 
-echo test
+country=$(sort-package-json "package.json" --check )
+
 # if [ $? -eq 0 ]
 # then
 #     echo 'Each package was already sorted.'
