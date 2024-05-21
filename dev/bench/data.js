7b6621a0bca1ec8ad4e4561be5b206738b40b3de
github-action-benchmark <github@users.noreply.github.com>
2024-05-21 15:59:51 +0000

    add Benchmark (benchmarkjs) benchmark result for d3589baa4bf782c9e25ece5c066ccaf1044dbe1f

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1716307190942,
+  "repoUrl": "https://github.com/rwat17/cactus",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "9387513+outSH@users.noreply.github.com",
+            "name": "Michal Bajer",
+            "username": "outSH"
+          },
+          "distinct": false,
+          "id": "d3589baa4bf782c9e25ece5c066ccaf1044dbe1f",
+          "message": "build: bump the go_modules group across 9 directories with 2 updates\n\nBumps the go_modules group with 1 update in the /weaver/common/protos-go directory: google.golang.org/protobuf.\nBumps the go_modules group with 2 updates in the /weaver/samples/fabric/go-cli directory: google.golang.org/protobuf and [github.com/prometheus/client_golang](https://github.com/prometheus/client_golang).\nBumps the go_modules group with 1 update in the /weaver/samples/fabric/satpsimpleasset directory: google.golang.org/protobuf.\nBumps the go_modules group with 1 update in the /weaver/samples/fabric/simpleasset directory: google.golang.org/protobuf.\nBumps the go_modules group with 1 update in the /weaver/samples/fabric/simpleassetandinterop directory: google.golang.org/protobuf.\nBumps the go_modules group with 1 update in the /weaver/samples/fabric/simpleassettransfer directory: google.golang.org/protobuf.\nBumps the go_modules group with 1 update in the /weaver/samples/fabric/simplestate directory: google.golang.org/protobuf.\nBumps the go_modules group with 1 update in the /weaver/samples/fabric/simplestatewithacl directory: google.golang.org/protobuf.\nBumps the go_modules group with 1 update in the /weaver/sdks/fabric/go-sdk directory: google.golang.org/protobuf.\n\nUpdates `google.golang.org/protobuf` from 1.30.0 to 1.33.0\n\nUpdates `google.golang.org/protobuf` from 1.31.0 to 1.33.0\n\nUpdates `github.com/prometheus/client_golang` from 1.3.0 to 1.11.1\n- [Release notes](https://github.com/prometheus/client_golang/releases)\n- [Changelog](https://github.com/prometheus/client_golang/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/prometheus/client_golang/compare/v1.3.0...v1.11.1)\n\nUpdates `google.golang.org/protobuf` from 1.30.0 to 1.33.0\n\nUpdates `google.golang.org/protobuf` from 1.30.0 to 1.33.0\n\nUpdates `google.golang.org/protobuf` from 1.30.0 to 1.33.0\n\nUpdates `google.golang.org/protobuf` from 1.30.0 to 1.33.0\n\nUpdates `google.golang.org/protobuf` from 1.30.0 to 1.33.0\n\nUpdates `google.golang.org/protobuf` from 1.30.0 to 1.33.0\n\nUpdates `google.golang.org/protobuf` from 1.31.0 to 1.33.0\n\n---\nupdated-dependencies:\n- dependency-name: google.golang.org/protobuf\n  dependency-type: direct:production\n  dependency-group: go_modules\n- dependency-name: google.golang.org/protobuf\n  dependency-type: direct:production\n  dependency-group: go_modules\n- dependency-name: github.com/prometheus/client_golang\n  dependency-type: indirect\n  dependency-group: go_modules\n- dependency-name: google.golang.org/protobuf\n  dependency-type: indirect\n  dependency-group: go_modules\n- dependency-name: google.golang.org/protobuf\n  dependency-type: indirect\n  dependency-group: go_modules\n- dependency-name: google.golang.org/protobuf\n  dependency-type: indirect\n  dependency-group: go_modules\n- dependency-name: google.golang.org/protobuf\n  dependency-type: indirect\n  dependency-group: go_modules\n- dependency-name: google.golang.org/protobuf\n  dependency-type: indirect\n  dependency-group: go_modules\n- dependency-name: google.golang.org/protobuf\n  dependency-type: indirect\n  dependency-group: go_modules\n- dependency-name: google.golang.org/protobuf\n  dependency-type: direct:production\n  dependency-group: go_modules\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nSigned-off-by: Sandeep Nishad <sandeep.nishad1@ibm.com>",
+          "timestamp": "2024-05-21T11:02:32+02:00",
+          "tree_id": "4921421dc89255f1bb7fee466bda8cd0586f55d7",
+          "url": "https://github.com/rwat17/cactus/commit/d3589baa4bf782c9e25ece5c066ccaf1044dbe1f"
+        },
+        "date": 1716307189125,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 575,
+            "range": "±1.74%",
+            "unit": "ops/sec",
+            "extra": "177 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 356,
+            "range": "±1.25%",
+            "unit": "ops/sec",
+            "extra": "180 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
